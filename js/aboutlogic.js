8d79f7f984d1605382d91953195a83c9389c711a
ashraygupta001 <ashray79gupta@gmail.com>
2019-05-23 01:55:33 +0530

    added team 50%

--- js/aboutlogic.js
+++ js/aboutlogic.js
@@ -7,3 +7,25 @@
 			}
 		}, 400);
 	});
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 2
+            }
+        }]
+    });
+});
