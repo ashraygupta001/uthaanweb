dd2e69c329c53558c845b841fbce30f5e3c47b0a
aditisingh2362 <aditisingh2362@gmail.com>
2019-07-28 10:29:50 +0530

    Changed titles

--- resources/views/articleindex.blade.php
+++ resources/views/articleindex.blade.php
@@ -71,13 +71,13 @@ html {
                 </div>          
          </div>
     </nav>  
-    <div class="hero-wrap js-fullheight" style="background-image: url('images/bg_3.jpg');" data-stellar-background-ratio="0.5">
+    <div class="hero-wrap js-fullheight" style="background-image: url('images/portfolio/05.jpg');" data-stellar-background-ratio="0.5">
       <div class="overlay"></div>
       <div class="container">
         <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center" data-scrollax-parent="true">
           <div class="col-md-9 text-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
             <p class="breadcrumbs" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><span class="mr-2"><a href="index.html">Home</a></span> <span>Articles</span></p>
-            <h1 class="mb-3 bread" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Post with right sidebar</h1>
+            <h1 class="mb-3 bread" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Articles</h1>
           </div>
         </div>
       </div>
